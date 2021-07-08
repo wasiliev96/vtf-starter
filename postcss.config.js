56a2ba01ba51c6fdcc53c3adfaadd7b3ac4f8af4
wasiliev96 <wasiliev.clooud@gmail.com>
2021-07-08 16:26:41 +0300

    add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
+    'postcss-import': {},
     'postcss-preset-env': {
       stage: 1, // early WD
       browsers: 'last 2 versions',
       autoprefixer: { grid: true },
-      features: {
-
-      }
+      features: {},
     },
   },
 }
