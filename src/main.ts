d7ee54f86367531f7f993fe1fdf4c3b6b33350f7
wasiliev96 <wasiliev.clooud@gmail.com>
2021-07-08 17:30:55 +0300

    format

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-
-
-createApp(App)
-.use(router)
-.mount('#app')
+createApp(App).use(router).mount('#app')
