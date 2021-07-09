0268abdb3a416656e90a4aecaee46bdfa938229f
wasiliev96 <wasiliev.clooud@gmail.com>
2021-07-09 14:25:16 +0300

    add navigaiton menu

--- src/components/layout/default/DefaultHeader.vue
+++ src/components/layout/default/DefaultHeader.vue
@@ -1,23 +1,19 @@
 <template>
-  <header class="p-2 flex justify-between">
+  <header class="flex justify-between">
     <div class="header-branding">
       <h1 class="text-center">This is default header</h1>
     </div>
     <div class="header-nav flex items-center">
       <main-navigation />
-      <button class="flex justify-center items-center p-3 hover:text-red-200 transition-colors">
-        <dots-vertical-icon class="h-5 w-5" />
-      </button>
     </div>
   </header>
 </template>
 
 <script>
-import { DotsVerticalIcon } from '@heroicons/vue/outline'
 import { defineComponent } from 'vue'
 import MainNavigation from './MainNavigation.vue'
 export default defineComponent({
-  components: { MainNavigation, DotsVerticalIcon },
+  components: { MainNavigation },
   name: 'DefaultHeader',
   setup() {
     return {}
--- src/components/layout/default/MainNavigation.vue
+++ src/components/layout/default/MainNavigation.vue
@@ -1,25 +1,68 @@
 <template>
-  <nav class="p-3">
-    <ul class="flex gap-2">
-      <li>
-        <router-link class="border-r py-2 pl-2 pr-4 text-center" to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link class="border-r py-2 pl-2 pr-4 text-center" to="/about">About</router-link>
-      </li>
-    </ul>
-  </nav>
+  <div class="dropdown flex justify-end relative">
+    <button ref="dropdownToggler" class="dropdown-toggler w-8 h-8 flex justify-center items-center">
+      <dots-vertical-icon class="h-5 w-5" />
+    </button>
+
+    <nav
+      class="
+        transition-all
+        ease-in-out
+        transform
+        origin-top-right
+        translate-x-2
+        scale-90
+        dropdown-menu
+        invisible
+        opacity-0
+        absolute
+        top-0
+        right-0
+        p-3
+        bg-white
+        border
+        rounded
+      "
+    >
+      <ul class="flex flex-col gap-2">
+        <li class="px-4 py-2 leading-5 text-gray-700" @click="blurDropdown">
+          <router-link class="" to="/">Home</router-link>
+        </li>
+        <li class="px-4 py-2 leading-5 text-gray-700">
+          <router-link class="" to="/about">About</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue'
+import { DotsVerticalIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
   name: 'MainNavigation',
+  components: {
+    DotsVerticalIcon,
+  },
   setup() {
     return {}
+  },
+  watch: {
+    $route(to, from) {
+      this.$refs.dropdownToggler.blur()
+    },
   },
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="postcss">
+.dropdown-toggler:focus {
+  color: var(--color-accent);
+  & + .dropdown-menu {
+    opacity: 1;
+    transform: translate(0) scale(1);
+    visibility: visible;
+  }
+}
+</style>
